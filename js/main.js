a1170c286fac50d16560f86112e08dfcbca4d63f
Rizki Akmanda <rizkiakmanda@gmail.com>
2014-07-12 22:22:15 +0700

    1.0 Version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery(window).load(function(){
+
+jQuery(".flex-wrapper .flexslider").flexslider( {
+		               slideshow : true,
+		               animation : "fade",
+		               pauseOnHover: true,
+		               animationSpeed : 400,
+		               smoothHeight : true,
+		               directionNav: true,
+		               controlNav: false
+		        			});
+
+});
+
+
